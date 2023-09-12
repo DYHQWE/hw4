1ae15b805ad9796c4f431d565a1ee72f17d9c296
DYHQWE <2419799397@qq.com>
2023-09-12 19:10:32 +0800

    hw4

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use runtime::{block_on, spawn};
+use std::time::Duration;
+use async_channel::{self};
+use async_std;
+
+async fn _demo1() {
+    println!("Hello");
+}
+
+async fn _demo2() {
+    let (tx, rx) = async_channel::bounded(1);
+    std::thread::spawn(move || {
+        std::thread::sleep(Duration::new(2, 0));
+        tx.send_blocking("world")
+    });
+    println!("hello");
+    let s = rx.recv().await.unwrap();
+    println!("{}", s);
+}
+
+async fn _demo3() {
+    spawn(_demo4());
+    async_std::task::sleep(Duration::new(2, 0)).await;
+    println!("Hello World!");
+}
+
+async fn _demo4() {
+    println!("Hello World2!");
+}
+
+fn main() {
+    block_on(_demo1());
+    block_on(_demo2());
+    block_on(_demo3());
+}
---
+++ src/run.rs
@@ -0,0 +1,146 @@
+use {
+    std::{
+        future::Future,
+        sync::{Condvar, Mutex, Arc},
+        task::{Wake, Waker, Context, Poll},
+        cell::RefCell,
+        collections::VecDeque,
+    },
+    futures::future::BoxFuture,
+};
+
+#[macro_use]
+extern crate scoped_tls;
+
+// Runnable queue中存放的是Task，一个Task对应一个Future
+struct Task {
+    future: RefCell<BoxFuture<'static, ()>>,
+    signal: Arc<Signal>,        /
+}
+
+unsafe impl Send for Task {}
+unsafe impl Sync for Task {}
+
+scoped_thread_local!(static SIGNAL: Arc<Signal>);
+// RUNNABLE中存放所有spawn出的从future
+scoped_thread_local!(static RUNNABLE: Mutex<VecDeque<Arc<Task>>>);
+
+impl Wake for Task {
+    fn wake(self: Arc<Self>) {
+        RUNNABLE.with(|runnable|
+            runnable.lock().unwrap().push_back(self.clone())
+        );
+        self.signal.notify();
+    }
+}
+
+
+// State of Executor
+struct Signal {
+    state: Mutex<State>,        // executor的状态
+    // executor没有可poll的future时，wait该条件变量
+    cond: Condvar,               
+}
+
+#[derive(Debug)]
+enum State {
+    Empty,
+    Waiting,
+    Notified,
+}
+
+impl Signal {
+    fn new() -> Self {
+        Signal {
+            state: Mutex::new(State::Empty),
+            cond: Condvar::new(),
+        }
+    }
+
+    // 当executor没有可poll的future时，调用Signal::wait让出CPU
+    fn wait(&self) {
+//        eprintln!("Signal::wait");
+        let mut state = self.state.lock().unwrap();
+        match *state {
+            State::Empty => {
+                *state = State::Waiting;
+                while let State::Waiting = *state {
+                    state = self.cond.wait(state).unwrap();
+                }
+            },
+            State::Waiting => {
+                panic!("multiple wait");
+            },
+            State::Notified => {
+                *state = State::Empty;
+            }
+        }
+    }
+
+    // 外部调用notify来唤醒executor
+    fn notify(&self) {
+//        eprintln!("Signal::notify");
+        let mut state = self.state.lock().unwrap();
+//        eprintln!("state before notify: {:?}", state);
+        match *state {
+            State::Empty => {
+                *state = State::Notified;
+            },
+            State::Waiting => {
+                *state = State::Empty;
+                self.cond.notify_one();
+            },
+            State::Notified => {}
+        }
+    }
+}
+
+// Signal可以作为Waker使用，对Waker::wake()最终落到Signal::notify
+impl Wake for Signal {
+    fn wake(self: Arc<Self>) {
+        // 当前executor只支持运行单个future，因此不需要维护runnable队列，这里只需要notify
+//        eprintln!("Signal::wake");
+        self.notify();
+    }
+}
+
+// Executor
+pub fn block_on<F: Future>(future: F) -> F::Output {
+    let mut main_fut = std::pin::pin!(future);
+    let signal = Arc::new(Signal::new());
+    let waker = Waker::from(signal.clone());
+    let mut cx = Context::from_waker(&waker);
+    let runnable : Mutex<VecDeque<Arc<Task>>> = Mutex::new(VecDeque::with_capacity(1024));
+    SIGNAL.set(&signal, || {
+        RUNNABLE.set(&runnable, || {
+            loop {
+                if let Poll::Ready(output) = main_fut.as_mut().poll(&mut cx) {
+                    return output;
+                }
+                while let Some(task) = runnable.lock().unwrap().pop_front() {
+                    let waker = Waker::from(task.clone());
+                    let mut cx = Context::from_waker(&waker);
+                    let _ = task.future.borrow_mut().as_mut().poll(&mut cx);
+                }
+                signal.wait();
+            }
+        })
+    })
+}
+
+pub fn spawn<F>(future: F)
+where
+    F: Future<Output = ()> + 'static + Send,
+{ 
+    let boxed_future = Box::pin(future);
+    SIGNAL.with(move |signal| {
+        RUNNABLE.with(move |runnable| {
+            let t = Task {
+                future: RefCell::new(boxed_future),
+                signal: signal.clone()
+            };
+            runnable.lock().unwrap().push_back(Arc::new(t));
+            signal.notify();
+        })
+    });
+}
